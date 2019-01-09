cb2961fc48b98aab9f25c4f29fc0d2930468e560
Phan <nphan20@vdc.csc.com>
2019-01-09 17:44:13 +0700

    upload project

---
+++ src/main/java/com/dxc/employee/EmployeeSalaryApplication.java
@@ -0,0 +1,14 @@
+package com.dxc.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSalaryApplication.class, args);
+	}
+
+}
+
